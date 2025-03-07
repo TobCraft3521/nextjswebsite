c6b5844d455b867d4dda75e92c92a6cf457ccb44
TobCraft3521 <tobiashackenberg@outlook.de>
2025-03-07 19:05:06 +0100

    next-sitemap + updated skills

--- components/elements/mobile-skills.tsx
+++ components/elements/mobile-skills.tsx
@@ -2,11 +2,17 @@ import {
   Atom,
   Brackets,
   Cpu,
+  Database,
+  DollarSign,
   Gauge,
+  GitFork,
   Github,
   HardDriveUpload,
   Hash,
+  Layers,
   ListMinus,
+  Server,
+  Users,
 } from "lucide-react"
 import Skill from "./skill"
 
@@ -14,37 +20,45 @@ const MobileSkills = () => {
   return (
     <div className="md:hidden p-8 space-y-8 mb-8">
       <div className="drop-shadow-2xl bg-white dark:bg-slate-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4">
-        <Skill icon={<Atom size={24} />} name="React/NextJS" progress={90} />
+        <Skill icon={<Atom size={24} />} name="Frontend/NextJS" progress={95} />
         <Skill
-          icon={<Brackets size={24} />}
-          name="Java-/Typescript"
+          icon={<Database size={24} />}
+          name="Backend & Database"
+          progress={85}
+        />
+        <Skill
+          icon={<Layers size={24} />}
+          name="Web Design & UI/UX"
+          progress={70}
+        />
+      </div>
+      <div className="drop-shadow-2xl bg-white dark:bg-slate-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4">
+        <Skill
+          icon={<Github size={24} />}
+          name="Git & Workflow Optimization"
           progress={90}
         />
         <Skill
-          icon={<Hash size={24} />}
-          name="Tailwind-CSS/Style"
-          progress={80}
+          icon={<GitFork size={24} />}
+          name="DevOps & CI/CD"
+          progress={65}
         />
-      </div>
-      <div className="drop-shadow-2xl bg-white dark:bg-slate-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4">
         <Skill
-          icon={<HardDriveUpload size={24} />}
-          name="Hosting/Linux/Computers"
-          progress={70}
-        />
-        <Skill icon={<Github size={24} />} name="Git/-hub" progress={85} />
-        <Skill
-          icon={<Gauge size={24} />}
-          name="Shorstcuts/Universal"
-          progress={85}
-        />
-      </div>
-      <div className="drop-shadow-2xl bg-white dark:bg-slate-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4">
-        <Skill icon={<ListMinus size={24} />} name="Python" progress={60} />
-        <Skill
-          icon={<Cpu size={24} />}
-          name="Hardware/Tech-stuff"
+          icon={<Server size={24} />}
+          name="Deployment & Cloud"
           progress={50}
+        />
+      </div>
+      <div className="drop-shadow-2xl bg-white dark:bg-slate-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4">
+        <Skill
+          icon={<Users size={24} />}
+          name="Teamwork & Organization"
+          progress={90}
+        />
+        <Skill
+          icon={<DollarSign size={24} />}
+          name="SaaS & Marketing"
+          progress={60}
         />
       </div>
     </div>
--- components/elements/skills.tsx
+++ components/elements/skills.tsx
@@ -3,11 +3,17 @@ import {
   Atom,
   Brackets,
   Cpu,
+  Database,
+  DollarSign,
   Gauge,
+  GitFork,
   Github,
   HardDriveUpload,
   Hash,
+  Layers,
   ListMinus,
+  Server,
+  Users,
 } from "lucide-react"
 import Skill from "./skill"
 import { useEffect, useState } from "react"
@@ -29,57 +35,66 @@ const Skills = () => {
   }, [])
   return (
     <div className="absolute top-[20vh] right-0 w-[60vw] hidden lg:flex">
-      {/* <h1 className="text-2xl nunito-font translate-x-[16vw] text-[rgb(10,37,64)] md:text-white mb-10 font-bold">
-        MY TECH SKILLS
-      </h1> */}
       <div className="w-[60vw] relative">
         <div
           style={{
             transform: `translateX(${scrollPosition}px)`,
           }}
-          className="drop-shadow-2xl skills bg-white dark:bg-zinc-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4 absolute left-[2vw]"
+          className="drop-shadow-2xl skills bg-white dark:bg-zinc-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4 absolute left-[5vw]"
         >
-          <Skill icon={<Atom size={24} />} name="React/NextJS" progress={90} />
           <Skill
-            icon={<Brackets size={24} />}
-            name="Java-/Typescript"
+            icon={<Atom size={24} />}
+            name="Frontend/NextJS"
             progress={90}
           />
           <Skill
-            icon={<Hash size={24} />}
-            name="Tailwind-CSS/Style"
-            progress={80}
+            icon={<Database size={24} />}
+            name="Backend & Database"
+            progress={85}
+          />
+          <Skill
+            icon={<Layers size={24} />}
+            name="Web Design & UI/UX"
+            progress={70}
           />
         </div>
         <div
           style={{
             transform: `translateX(${scrollPosition * 2.5}px)`,
           }}
-          className="drop-shadow-2xl skills2 bg-white dark:bg-zinc-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4 absolute top-[4vh] left-[28vw]"
+          className="drop-shadow-2xl skills2 bg-white dark:bg-zinc-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4 absolute top-[4vh] left-[32vw]"
         >
           <Skill
-            icon={<HardDriveUpload size={24} />}
-            name="Hosting/Linux/Computers"
-            progress={70}
+            icon={<Github size={24} />}
+            name="Git & Workflow Optimization"
+            progress={90}
           />
-          <Skill icon={<Github size={24} />} name="Git/-hub" progress={85} />
           <Skill
-            icon={<Gauge size={24} />}
-            name="Shorstcuts/Universal"
-            progress={85}
+            icon={<GitFork size={24} />}
+            name="DevOps & CI/CD"
+            progress={65}
+          />
+          <Skill
+            icon={<Server size={24} />}
+            name="Deployment & Cloud"
+            progress={50}
           />
         </div>
         <div
           style={{
             transform: `translateX(${scrollPosition * 2}px)`,
           }}
-          className="drop-shadow-2xl skills3 bg-white dark:bg-zinc-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4 absolute top-[42vh] left-[8vw]"
+          className="drop-shadow-2xl skills3 bg-white dark:bg-zinc-950 rounded-3xl lg:w-[22vw] p-8 border-slate-200 dark:border-slate-800 border-[1px] space-y-4 absolute top-[38vh] left-[8vw]"
         >
-          <Skill icon={<ListMinus size={24} />} name="Python" progress={50} />
           <Skill
-            icon={<Cpu size={24} />}
-            name="Hardware/Tech-stuff"
-            progress={40}
+            icon={<Users size={24} />}
+            name="Teamwork & Organization"
+            progress={90}
+          />
+          <Skill
+            icon={<DollarSign size={24} />}
+            name="SaaS & Marketing"
+            progress={60}
           />
         </div>
       </div>
@@ -88,32 +103,3 @@ const Skills = () => {
 }
 
 export default Skills
-
-/* <Skill icon={<Atom size={24} />} name="React/NextJS" progress={90} />
-<Skill
-  icon={<Brackets size={24} />}
-  name="Java-/Typescript"
-  progress={90}
-/>
-<Skill
-  icon={<Hash size={24} />}
-  name="Tailwind-CSS/Style"
-  progress={80}
-/>
-<Skill
-  icon={<HardDriveUpload size={24} />}
-  name="Hosting/Linux/Computers"
-  progress={70}
-/>
-<Skill icon={<Github size={24} />} name="Git/-hub" progress={85} />
-<Skill
-  icon={<Gauge size={24} />}
-  name="Shorstcuts/Universal"
-  progress={85}
-/>
-<Skill icon={<ListMinus size={24} />} name="Python" progress={60} />
-<Skill
-  icon={<Cpu size={24} />}
-  name="Hardware/Tech-stuff"
-  progress={50}
-/> */
